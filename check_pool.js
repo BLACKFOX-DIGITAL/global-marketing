37db26dcc29faae307ec8d3c28e83ac6d91c2fa0
Tanvir Mahedi <shakkhor666@Tanvirs-MacBook-Air.local>
2026-03-18 22:06:06 +0600

    Clean up old temporary debug scripts and logs

--- check_pool.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-p.lead.count({ where: { ownerId: null } })
-  .then(c => console.log('Unassigned leads in pool:', c))
-  .catch(e => console.error(e))
-  .finally(() => p.$disconnect());
