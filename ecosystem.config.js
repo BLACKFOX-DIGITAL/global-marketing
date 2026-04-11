41b7fa920c07963b7b6450da419ed9b12e5842c9
Tanvir Mahedi <shakkhor666@Tanvirs-MacBook-Air.local>
2026-04-11 23:22:35 +0600

    Update ecosystem config with port 3010

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,12 @@ module.exports = {
     {
       name: "global-marketing",
       script: "npm",
-      args: "start",
+      args: "start -- -p 3010",
       instances: 2,
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
+        PORT: 3010
       }
     }
   ]
